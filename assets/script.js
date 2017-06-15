8b3e25221767da2357014ad4e47509f14530f333
micelle <micelle_yamame@hotmail.co.jp>
2017-06-15 20:01:15 +0900

    no message

---
+++ assets/script.js
@@ -0,0 +1,315 @@
+/* ********************
+　バージョン 2.01
+　更新日 2017/06/14
+
+　更新履歴
+2017/04/26
+　公開
+2017/04/27
+　フェニックスの画像がファフニールになってたので修正
+　JavaScriptのキャッシュを保持させるためにクエリー文字列を追加
+2017/05/08
+　画像化される範囲を拡大（名称タイトルを含むようにしました）
+　またURLを画像へ表示させるよう調整
+2017/05/10
+　コードの調整
+　推しキャラ機能実装
+2017/05/16
+　Firefoxだと3凸ボタンが効かなかったため修正
+　http://www.searchlight8.com/firefox-event-preventdefault/
+2017/05/22
+　「推しキャラ分かりづらい」とのコメントがあったので、それっぽく修正
+2017/05/25
+　SSL対応（HTTPS化）により微調整をいたしました
+2017/06/02
+　ソースコードを大幅に変更（Ver2.0）
+　JSONを使って各種リスト生成・テーブル生成するように変更しました
+2017/06/14
+　ソースコードの細かい調整
+　GitHubで管理を始めた…？
+******************** */
+$(function(){
+
+/* ********************
+　初期設定
+******************** */
+// 変数設定
+var $summon_screeen=$('#summon_screeen');
+var alldata=[]; // すべてのデータを保持しておく配列
+var filterdata=[]; // フィルターを掛けたデータを保持する配列
+$.getJSON('assets/echo',init); // JSONを取得
+//$.getJSON('https://script.google.com/macros/s/AKfycbxo5Ny0LL8HcNvE4QH8NdrBwYQW8gsX3Nkpp-SHPshtyjqLP6U/exec', init);
+function init(data){
+	alldata=data; // 全データを保持しておく
+	filterdata=alldata; // とりあえず全データを代入しておく
+	$('#summon_setting div[class^="type"]').each(function(){
+		var $this_parent_class=$(this).attr('class');
+		list_display($this_parent_class); // リスト生成
+		radio_display($this_parent_class); // 3凸・4凸ボタン
+	});
+	table_display(); // 一覧表示
+}
+/* ********************
+　リスト生成の関数
+******************** */
+function list_display($this_parent_class){
+	var $this_parent_class=$('#summon_setting .'+$this_parent_class);
+	var ntype=$this_parent_class.attr('class');
+	var nselect=$this_parent_class.data('select');
+	var vtype=$this_parent_class.find('[name=c_type]').val();
+	var vrarity=$this_parent_class.find('[name=c_rarity]').val();
+	for(var n=0; n<filterdata.length; n++){
+		var ftype=filterdata[n].type;
+		var frarity=filterdata[n].rarity;
+		var fclass=filterdata[n].class;
+		var fname=filterdata[n].name;
+		var fid=filterdata[n].id;
+		var fselected=(function() {
+			if(
+				fname.match(/マグナ/)&&ntype.match(/[1-7]/) ||
+				fname.match(/グランデ/)&&ntype.match(/8/) ||
+				fname.match(/ホワイトラビット/)
+			) return 'selected';
+			return '';
+		})();
+		if (ftype.match(/type0/) && frarity===vrarity && fclass.match(nselect)){
+			$('.type8 .c_summon').append('<option value='+fid+'>'+fname+'</option>');
+		} else if (ftype.match(vtype) && frarity===vrarity && fclass.match(nselect)){
+			$('.'+ntype+' .c_summon').append('<option value='+fid+' '+fselected+'>'+fname+'</option>');
+		}
+	}
+}
+/* ********************
+　3凸・4凸ボタンを有効化・無効化の関数
+******************** */
+function radio_display($this_parent_class){
+	var $this_parent_class=$('#summon_setting .'+$this_parent_class);
+	var vc_summon=$this_parent_class.find('.c_summon').val();
+	for(var n=0; n<filterdata.length; n++){
+		if (filterdata[n].id.match(vc_summon)){
+			var $label_0=$this_parent_class.find('.radio label:eq(0)'); // 3凸のボタン（ラベル）
+			var $label_1=$this_parent_class.find('.radio label:eq(1)'); // 4凸のボタン（ラベル）
+			var $input_0=$this_parent_class.find('.radio input:eq(0)'); // 3凸のボタン（チェック）
+			var $input_1=$this_parent_class.find('.radio input:eq(1)'); // 4凸のボタン（チェック）
+			// 3凸の表示＆選択、4凸の非表示＆選択を外す
+			$label_0.css('display', '');
+			$label_1.css('display', 'none');
+			$input_0.prop('checked', true);
+			$input_1.prop('checked', false);
+			if (filterdata[n].rank4.length > 1) { 
+				$label_1.css('display', ''); // 4凸を表示
+			} else if (filterdata[n].rank3.length===0) {
+				$input_0.prop('checked', false); // 3凸の選択を外す
+			}
+		}
+	}
+}
+/* ********************
+　テーブルへ画像・文字を表示させる関数
+******************** */
+function table_display(){
+	$('.c_summon').each(function() {
+		var summon_select=$(this).val(); // 召喚石のID取得
+		var summon_rank=$(this).nextAll('.radio').find('input:radio:checked').val(); // Rank取得
+		var summon_type=$(this).parent().attr('class'); // 親要素取得
+		var job_rarity = $('.type9 [name=c_type]').val(); // ジータかグランか
+		var type_icon=$('.setting .type8 .title').hasClass('type_icon');
+
+		// 設定画面に.type_iconがあったら
+		if (type_icon) {
+			$summon_screeen.find('.type8 .title').addClass('type_icon').html('フリー属性固定召喚石');
+			$('.type8 .content').removeClass('breakword');
+		// 設定画面に.type_iconがなかったら
+		} else if (!type_icon) {
+			$summon_screeen.find('.type8 .title').removeClass('type_icon').html('推しキャラ');
+			$('.type8 .content').addClass('breakword');
+			// エスケープ http://www.webdesignleaves.com/wp/htmlcss/1485/
+			var targets = ["&", "<", ">" ,'"', "'"]; // エスケープする文字はこれ！
+			var escapes = ["&amp;", "&lt;", "&gt;", "&quot;", "&#39;"]; // 変換先のジョブはこれ！
+			var converted = $('textarea[name="comment"]').val(); // 入力された文字取得をするのだ
+			for(var i=0; i<targets.length; i++){ // forでフォ～～～っと変換するのだ
+				converted = converted.replace(new RegExp(targets[i], 'g'), escapes[i]);
+			}
+			$('.type8 .spec').html(converted).removeClass('rank0 rank3 rank4'); // 書き込みじゃ
+		}
+
+		for(var n=0; n<filterdata.length; n++){
+			//var fname=filterdata[n].name;
+			//var ftype = filterdata[n].type;
+			var fclass=filterdata[n].class;
+			var frarity=filterdata[n].rarity;
+			var fid=filterdata[n].id;
+			if (fid.match(summon_select)){
+				// 召喚石・キャラ・JOBの画像
+				var _frarity = (function() {
+					if(fclass.match(/^summon$/)||fclass.match(/^character$/)) return '_'+frarity;
+					return '';
+				})();
+				var _extension = (function() {
+					if (fclass.match(/^character$/)) {
+						if(summon_rank===void 0) return '_01.jpg';
+						if(summon_rank.match(/^rank3$/)) return '_02.jpg';
+						if(summon_rank.match(/^rank4$/)) return '_03.jpg';
+					}
+					if (fclass.match(/^job$/)) {
+					if(job_rarity.match(/^Djeeta$/)) return '_1_01.png';
+					if(job_rarity.match(/^Gran$/)) return '_0_01.png';
+					}
+					return '.jpg';
+				})();
+				$summon_screeen.find('.'+summon_type+' img').attr('src', 'image/'+fclass+ _frarity +'/'+fid+ _extension); // 画像の書き換え
+				// 召喚石の文章
+				var _summon_rank = (function() {
+					if (summon_rank === void 0) return 'rank0';
+					return summon_rank;
+				})();
+				$summon_screeen.find('.'+summon_type+' .spec').html(filterdata[n][_summon_rank]).removeClass('rank0 rank3 rank4').addClass(_summon_rank); // 文章・Class処理
+			}
+		}
+		
+
+		// ユーザーID取得＆書き込み
+		var job_id = parseInt($('input[name="user_id"]').val());
+		$('.type9 .spec').html('<div>'+job_id+'</div>');
+
+	});
+}
+
+/* ********************
+　変更するボタンをクリックしたら、テーブルを表示
+******************** */
+$('.change').on('click', function (){
+	table_display();
+});
+/* ********************
+　プルダウンが変わったら
+　3凸・4凸ボタンを有効化・無効化の関数へ
+******************** */
+$('div').on('change', '.c_summon', function() {
+	var $this_parent_class=$(this).parent().attr('class');
+	radio_display($this_parent_class);
+});
+/* ********************
+　属性,レアリティのプルダウンが変わったら
+******************** */
+$('div').on('change', 'div [name=c_type], div [name=c_rarity]', function() {
+	var $parent=$(this).parent();
+	var $starget=$parent.find('.c_summon');
+	var $vtype=$parent.find('[name=c_type]');
+	var vtype=$vtype.val();
+	var $this_parent_class=$parent.attr('class');
+	$starget.empty(); // とりま召喚石リストを削除
+	list_display($this_parent_class); //リスト作成
+	radio_display($this_parent_class);//ラジオボタン
+	// 属性それぞれ色を変える
+	switch(vtype){
+		case 'type1':$vtype.css('color', '#FF0000');break;
+		case 'type2':$vtype.css('color', '#00FFFF');break;
+		case 'type3':$vtype.css('color', '#FF9872');break;
+		case 'type4':$vtype.css('color', '#00FF00');break;
+		case 'type5':$vtype.css('color', '#FFFF00');break;
+		case 'type6':$vtype.css('color', '#FF00FF');break;
+		//default:$vtype.css('color', '');break;
+	}
+});
+/* ********************
+　フリー・推しチェンジの処理
+******************** */
+$('.setting .type8 .title').on('click', function () { // フリー2をクリックしたとき
+	var $this=$(this);
+	var $html=$this.html();
+	var $c_type=$this.nextAll('.c_type');
+	var $c_rarity=$this.nextAll('.c_rarity');
+	var $radio=$this.nextAll('.radio');
+	var $nclass=$this.hasClass('type_icon');
+	var $this_parent_class=$this.parent().attr('class');
+	$c_rarity.find('option').prop('selected', false);
+	$('.type8 .c_summon').empty();
+	$c_rarity.empty();
+	/* ********************
+	　.type_iconがあったら「推しキャラ」の設定にする
+	******************** */
+	if ($nclass) {
+		$this.removeClass('type_icon').html($html.replace(/フリー属性2/g,'推しキャラ'));
+		$c_type.css('color', '#FF0000').attr("selected", false).val('type1');
+		$c_rarity.append(
+			'<option value="ssr">SSR</option>'+
+			'<option value="sr">SR</option>'+
+			'<option value="r">R</option>'+
+			'<option value="skin">スキン他</option>'
+		);
+		$this.parent().attr('data-select','character').data('select','character');
+		$radio.find('.r_rank3').text('上限解放');
+		$radio.find('.r_rank4').text('最終上限解放').css({'line-height':'16px','height':'16px','font-size':'12px'});
+		$this.parent().append('<textarea type="text" name="comment" placeholder="45文字まで入力可能です" maxlength="45">');
+		list_display($this_parent_class); //リスト作成
+	/* ********************
+	　.type_iconがなかったら「召喚石」の設定にする
+	******************** */
+	} else if (!$nclass) {
+		$this.addClass('type_icon').html($html.replace(/推しキャラ/g,'フリー属性2'));
+		$c_type.css('color', '#FFFF00').attr("selected", false).val('type5');
+		$c_rarity.append(
+			'<option value="ssr">SSR</option>'+
+			'<option value="sr">SR</option>'
+		);
+		$this.parent().attr('data-select','summon').data('select','summon');
+		$radio.find('.r_rank3').text('3凸');
+		$radio.find('.r_rank4').text('4凸').css({'line-height':'','height':'','font-size':''});
+		$this.nextAll('textarea').remove();
+		list_display($this_parent_class); //リスト作成
+	}
+});
+/* ********************
+　3凸・4凸ボタンの動作
+　http://hueruwakame.php.xdomain.jp/article/html_css3.php
+******************** */
+$('div').on('click', '.radio label', function(event) {
+	// 既定の動作をキャンセル(今回はinputにcheckedが入るのをキャンセル)
+	event.preventDefault();
+	for(var n=0; n<filterdata.length; n++){
+		if (filterdata[n].id.match($(this).parent().parent().find('.c_summon').val())){
+			if (filterdata[n].rank0.length===0 || filterdata[n].rank3.length===0) { // 無凸、3凸がある
+			} else {
+				var $input = $(this).prev('input');
+				if ( $input.prop('checked') ) {
+					$input.prop('checked', false);
+				} else {
+					$input.prop('checked', true);
+				}
+			}
+		}
+	}
+});
+/* ********************
+　画像化の処理
+******************** */
+// 画像化したい要素をセレクタに指定
+//var $summon_screeen=$('#summon_screeen');
+$('#screenshot').on('click', function () {
+	// 属性アイコンが正常に描写されないので非表示
+	$summon_screeen.find('.title').removeClass('type_icon');
+	// サイトのURLを記載させる
+	$summon_screeen.append('<p class="add">https://prfac.com/gbf/summon/</p>');
+	// 画像生成を開始
+	html2canvas($summon_screeen, {
+		onrendered: function (canvas) {
+		var imgData = canvas.toDataURL();
+			$('#screen_image').attr('src', imgData);
+			$('#download_screen').attr('download', 'image.png').attr('href', imgData);
+		}
+	});
+	setTimeout(function(){
+		// 属性アイコンを0.1秒後に再表示させます
+		$summon_screeen.find('.content').each(function(){
+			if (!$(this).hasClass('breakword')) {
+				$(this).prev().addClass('type_icon');
+			}
+		});
+		// 記載させたURLを削除
+		$summon_screeen.find('.add').remove();
+	},100);
+});
+
+//JSON 終了
+});
---
+++ index.php
@@ -0,0 +1,355 @@
+<?php
+function getFileUpdateDate($file) {
+	$FUDate = "";
+	$FUDate = filemtime($file);
+	$FUDate = date("YmdHi", $FUDate);
+	$FUDate = $file."?date=".$FUDate;
+	return $FUDate;
+}
+function getLastUpdateDate() {
+	$weekday = array( "日", "月", "火", "水", "木", "金", "土" );
+	chdir("assets");
+	$file = exec("ls -tr");
+	$LUDate = "";
+	$LUDate = filemtime($file);
+	chdir("../");
+	$LUDate_m = date("m", $LUDate)."月";
+	$LUDate_d = date("d", $LUDate)."日";
+	$LUDate_w = "(".$weekday[date("w", $LUDate)].")";
+	$LUDate = $LUDate_m.$LUDate_d.$LUDate_w;
+	return $LUDate;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="jp">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:site" content="@micelle9" />
+	<meta name="twitter:title" content="フレ石編成的ななにか" />
+	<meta name="twitter:description" content="グラブルのフレ募集とかで使えそうな「フレ石編成的ななにか」です" />
+	<meta name="twitter:image" content="https://prfac.com/wp-content/uploads/2017/05/0d40a5e4a645fc6b96e767d64ac0878e.jpg" />
+
+	<link rel="shortcut icon" href="image/icon.png">
+	<link rel="stylesheet" href="<?php
+		$file= "assets/style.css";
+		$FUDate = getFileUpdateDate($file);
+		print $FUDate;
+	?>">
+	<title>フレ石編成的ななにか</title>
+</head>
+
+<body>
+	<div id="menu">
+		<ul>
+		<li><a href="../../">ブログ</a></li>
+		<li><a href="../../gbf-summon-usage/">使い方</a></li>
+		<li><a href="../../gbf-summon-usage/#history">更新履歴</a></li>
+		<li><a href="https://twitter.com/micelle9">お問い合わせ</a></li>
+		</ul>
+	</div>
+
+	<div class="table setting">
+		<p>最終更新：<?php
+			$LUDate = getLastUpdateDate();
+			print $LUDate;
+		?><br>情報が更新されない場合はキャッシュの削除をして下さい。</p>
+	</div>
+
+	<div class="table" id="summon_screeen">
+		<h2>フレ石編成的ななにか</h2>
+		<div class="box">
+			<div class="type1">
+				<div class="title type_icon">火属性固定召喚石</div>
+				<div class="content"><div class="image"><img></div><div class="spec"></div></div>
+			</div>
+			<div class="type2">
+				<div class="title type_icon">水属性固定召喚石</div>
+				<div class="content"><div class="image"><img></div><div class="spec"></div></div>
+			</div>
+			<div class="type3">
+				<div class="title type_icon">土属性固定召喚石</div>
+				<div class="content"><div class="image"><img></div><div class="spec"></div></div>
+			</div>
+			<div class="type4">
+				<div class="title type_icon">風属性固定召喚石</div>
+				<div class="content"><div class="image"><img></div><div class="spec"></div></div>
+			</div>
+			<div class="type5">
+				<div class="title type_icon">光属性固定召喚石</div>
+				<div class="content"><div class="image"><img></div><div class="spec"></div></div>
+			</div>
+			<div class="type6">
+				<div class="title type_icon">闇属性固定召喚石</div>
+				<div class="content"><div class="image"><img></div><div class="spec"></div></div>
+			</div>
+			<div class="type7">
+				<div class="title type_icon">フリー属性固定召喚石</div>
+				<div class="content"><div class="image"><img></div><div class="spec"></div></div>
+			</div>
+			<div class="type8">
+				<div class="title type_icon">フリー属性固定召喚石</div>
+				<div class="content"><div class="image"><img></div><div class="spec"></div></div>
+			</div>
+			<div class="type9">
+				<div class="title type_icon">ユーザーID</div>
+				<div class="content"><div class="image"><img></div><div class="spec job"></div></div>
+			</div>
+		</div>
+	</div>
+
+	<div class="table setting" id="summon_setting" onselectstart="return false;" unselectable="on">
+		<div class="box">
+			<div class="type1" data-select="summon">
+				<div class="title type_icon" style="border-bottom:none;">火属性</div>
+				<select class="c_type" name="c_type" style="color:#FF0000;">
+					<option value="type1" selected>火</option>
+					<option value="type2">水</option>
+					<option value="type3">土</option>
+					<option value="type4">風</option>
+					<option value="type5">光</option>
+					<option value="type6">闇</option>
+				</select>
+				<select class="c_rarity" name="c_rarity">
+					<option value="ssr">SSR</option>
+					<option value="sr">SR</option>
+					<!-- <option value="r">R</option> -->
+					<!-- <option value="n">N</option> -->
+				</select>
+				<select class="c_summon" name="type1"></select>
+				<div class="radio">
+					<input type="radio" name="type1" id="radio13" value="rank3" checked="">
+					<label for="radio13" class="r_rank3">3凸</label>
+					<input type="radio" name="type1" id="radio14" value="rank4">
+					<label for="radio14" class="r_rank4" style="display: none;">4凸</label>
+				</div>
+			</div>
+
+			<div class="type2" data-select="summon">
+				<div class="title type_icon" style="border-bottom:none;">水属性</div>
+				<select class="c_type" name="c_type" style="color:#00FFFF;">
+					<option value="type1">火</option>
+					<option value="type2" selected>水</option>
+					<option value="type3">土</option>
+					<option value="type4">風</option>
+					<option value="type5">光</option>
+					<option value="type6">闇</option>
+				</select>
+				<select class="c_rarity" name="c_rarity">
+					<option value="ssr">SSR</option>
+					<option value="sr">SR</option>
+					<!-- <option value="r">R</option> -->
+					<!-- <option value="n">N</option> -->
+				</select>
+				<select class="c_summon" name="type2"></select>
+				<div class="radio">
+					<input type="radio" name="type2" id="radio23" value="rank3" checked="">
+					<label for="radio23" class="r_rank3">3凸</label>
+					<input type="radio" name="type2" id="radio24" value="rank4">
+					<label for="radio24" class="r_rank4" style="display: none;">4凸</label>
+				</div>
+			</div>
+
+			<div class="type3" data-select="summon">
+				<div class="title type_icon" style="border-bottom:none;">土属性</div>
+				<select class="c_type" name="c_type" style="color:#FF9872;">
+					<option value="type1">火</option>
+					<option value="type2">水</option>
+					<option value="type3" selected>土</option>
+					<option value="type4">風</option>
+					<option value="type5">光</option>
+					<option value="type6">闇</option>
+				</select>
+				<select class="c_rarity" name="c_rarity">
+					<option value="ssr">SSR</option>
+					<option value="sr">SR</option>
+					<!-- <option value="r">R</option> -->
+					<!-- <option value="n">N</option> -->
+				</select>
+				<select class="c_summon" name="type3"></select>
+				<div class="radio">
+					<input type="radio" name="type3" id="radio33" value="rank3" checked="">
+					<label for="radio33" class="r_rank3">3凸</label>
+					<input type="radio" name="type3" id="radio34" value="rank4">
+					<label for="radio34" class="r_rank4" style="display: none;">4凸</label>
+				</div>
+			</div>
+
+			<div class="type4" data-select="summon">
+				<div class="title type_icon" style="border-bottom:none;">風属性</div>
+				<select class="c_type" name="c_type" style="color:#00FF00;">
+					<option value="type1">火</option>
+					<option value="type2">水</option>
+					<option value="type3">土</option>
+					<option value="type4" selected>風</option>
+					<option value="type5">光</option>
+					<option value="type6">闇</option>
+				</select>
+				<select class="c_rarity" name="c_rarity">
+					<option value="ssr">SSR</option>
+					<option value="sr">SR</option>
+					<!-- <option value="r">R</option> -->
+					<!-- <option value="n">N</option> -->
+				</select>
+				<select class="c_summon" name="type4"></select>
+				<div class="radio">
+					<input type="radio" name="type4" id="radio43" value="rank3" checked="">
+					<label for="radio43" class="r_rank3">3凸</label>
+					<input type="radio" name="type4" id="radio44" value="rank4">
+					<label for="radio44" class="r_rank4" style="display: none;">4凸</label>
+				</div>
+			</div>
+			<div class="type5" data-select="summon">
+				<div class="title type_icon" style="border-bottom:none;">光属性</div>
+				<select class="c_type" name="c_type" style="color:#FFFF00;">
+					<option value="type1">火</option>
+					<option value="type2">水</option>
+					<option value="type3">土</option>
+					<option value="type4">風</option>
+					<option value="type5" selected>光</option>
+					<option value="type6">闇</option>
+				</select>
+				<select class="c_rarity" name="c_rarity">
+					<option value="ssr">SSR</option>
+					<option value="sr">SR</option>
+					<!-- <option value="r">R</option> -->
+					<!-- <option value="n">N</option> -->
+				</select>
+				<select class="c_summon" name="type5"></select>
+				<div class="radio">
+					<input type="radio" name="type5" id="radio53" value="rank3" checked="">
+					<label for="radio53" class="r_rank3">3凸</label>
+					<input type="radio" name="type5" id="radio54" value="rank4">
+					<label for="radio54" class="r_rank4" style="display: none;">4凸</label>
+				</div>
+			</div>
+			<div class="type6" data-select="summon">
+				<div class="title type_icon" style="border-bottom:none;">闇属性</div>
+				<select class="c_type" name="c_type" style="color:#FF00FF;">
+					<option value="type1">火</option>
+					<option value="type2">水</option>
+					<option value="type3">土</option>
+					<option value="type4">風</option>
+					<option value="type5">光</option>
+					<option value="type6" selected>闇</option>
+				</select>
+				<select class="c_rarity" name="c_rarity">
+					<option value="ssr">SSR</option>
+					<option value="sr">SR</option>
+					<!-- <option value="r">R</option> -->
+					<!-- <option value="n">N</option> -->
+				</select>
+				<select class="c_summon" name="type6"></select>
+				<div class="radio">
+					<input type="radio" name="type6" id="radio63" value="rank3" checked="">
+					<label for="radio63" class="r_rank3">3凸</label>
+					<input type="radio" name="type6" id="radio64" value="rank4">
+					<label for="radio64" class="r_rank4" style="display: none;">4凸</label>
+				</div>
+			</div>
+			<div class="type7" data-select="summon">
+				<div class="title type_icon" style="border-bottom:none;">フリー属性1</div>
+				<select class="c_type" name="c_type" style="color:#FFFF00;">
+					<option value="type1">火</option>
+					<option value="type2">水</option>
+					<option value="type3">土</option>
+					<option value="type4">風</option>
+					<option value="type5" selected>光</option>
+					<option value="type6">闇</option>
+				</select>
+				<select class="c_rarity" name="c_rarity">
+					<option value="ssr">SSR</option>
+					<option value="sr" selected>SR</option>
+					<!-- <option value="r">R</option> -->
+					<!-- <option value="n">N</option> -->
+				</select>
+				<select class="c_summon" name="type7"></select>
+				<div class="radio">
+					<input type="radio" name="type7" id="radio73" value="rank3" checked="">
+					<label for="radio73" class="r_rank3">3凸</label>
+					<input type="radio" name="type7" id="radio74" value="rank4">
+					<label for="radio74" class="r_rank4" style="display: none;">4凸</label>
+				</div>
+			</div>
+			<div class="type8" data-select="summon">
+				<div class="title type_icon" style="border-bottom:none;">フリー属性2
+					<div class="float">
+						💬<span>クリックすることで<br>[フリー⇔推し]<br>を切り替えることができます</span>
+					</div>
+				</div>
+				<select class="c_type" name="c_type" style="color:#FFFF00;">
+					<option value="type1">火</option>
+					<option value="type2">水</option>
+					<option value="type3">土</option>
+					<option value="type4">風</option>
+					<option value="type5" selected>光</option>
+					<option value="type6">闇</option>
+				</select>
+				<select class="c_rarity" name="c_rarity">
+					<option value="ssr">SSR</option>
+					<option value="sr">SR</option>
+					<!-- <option value="r">R</option> -->
+					<!-- <option value="n">N</option> -->
+				</select>
+				<select class="c_summon" name="type8"></select>
+				<div class="radio">
+					<input type="radio" name="type8" id="radio83" value="rank3" checked="">
+					<label for="radio83" class="r_rank3">3凸</label>
+					<input type="radio" name="type8" id="radio84" value="rank4">
+					<label for="radio84" class="r_rank4" style="display: none;">4凸</label>
+				</div>
+			</div>
+			<div class="type9" data-select="job">
+				<div class="title type_icon" style="border-bottom:none;">ユーザーID</div>
+				<select class="c_type" name="c_type">
+					<option value="Djeeta">ジータ</option>
+					<option value="Gran">グラン</option>
+				</select>
+				<select class="c_rarity" name="c_rarity">
+					<option value="c4">Class IV</option>
+					<option value="c3">Class III</option>
+					<option value="c2">Class II</option>
+					<option value="c1" selected>Class I</option>
+					<option value="ex">Class Ex</option>
+					<option value="skin">Skin</option>
+				</select>
+				<select class="c_summon" name="type9"></select>
+				<input type="number" name="user_id" value="12345678">
+			</div>
+			<a class="change button">変更する</a>
+		</div>
+	</div>
+
+	<div class="table setting" onselectstart="return false;" unselectable="on">
+		<p>画像生成を行わないと、ダウンロードできません</p>
+		<div class="box">
+			<div><div id="screenshot" class="button">画像生成</div></div>
+			<div><span class="arrow"></span></div>
+			<div><a href="#"><div id="download_screen" class="button" >ダウンロード</div></a></div>
+		</div>
+	</div>
+	<div id="output_screen">
+		<p>以下のプレビューからも保存できます</p>
+		<img id="screen_image">
+	</div>
+
+	<div class="table setting">
+		<hr>
+		<p>なにかありましたら<a href="https://twitter.com/micelle9" target="_blank" style="color:#59b1eb;">Twitter</a>までご報告ください（´・ω・｀）</p>
+	</div>
+
+	<!-- 各種Script -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js"></script>
+	<script src="<?php
+		$file = "assets/script.js";
+		$FUDate = getFileUpdateDate($file);
+		print $FUDate;
+	?>"></script>
+	<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-56839189-1', 'auto');ga('send', 'pageview');</script>
+
+</body>
+
+</html>
