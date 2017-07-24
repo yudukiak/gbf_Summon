d6e8966c3ceabfe00512da7626a6a3a8193765aa
micelle <micelle_yamame@hotmail.co.jp>
2017-07-24 10:24:40 +0900

    [update] JSONの不要な箇所を削除する機能を追加
    これで加工の手間が減ります。

--- assets/echo.php
+++ assets/echo.php
@@ -7,12 +7,14 @@ function sanitize_output($buffer) {
 		'/[^\S ]+\</s',      // strip whitespaces before tags, except space
 		'/(\s)+/s',          // shorten multiple whitespace sequences
 		'/<!--[\s\S]*?-->/s', // コメントを削除
-		'/\s/s' // スペース全て削除
+		'/\s/s', // スペース全て削除
+		'/,{"class":"","rarity":"","type":"","id":"","name":"","rank0":"","rank3":"","rank4":""}/s' // JSONの不要な箇所を削除
 	);
 	$replace = array(
 		'>',
 		'<',
 		'\\1',
+		'',
 		'',
 		''
 	);
