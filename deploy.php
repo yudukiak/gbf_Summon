3059a843afcc156cca216603ee60a6dfe2eed386
micelle <micelle_yamame@hotmail.co.jp>
2017-07-25 13:30:24 +0900

    [add] 自動デプロイ機能を追加

---
+++ deploy.php
@@ -0,0 +1,28 @@
+<?php
+include '../gbf_summon.php';
+
+// http://isometriks.com/verify-github-webhooks-with-php
+$secret = $Git_Secret;
+
+$headers = getallheaders();
+$hubSignature = $headers['X-Hub-Signature'];
+
+// Split signature into algorithm and hash
+list($algo, $hash) = explode('=', $hubSignature, 2);
+
+// Get payload
+$payload = file_get_contents('php://input');
+
+// Calculate hash based on payload and the secret
+$payloadHash = hash_hmac($algo, $payload, $secret);
+
+// Check if hashes are equivalent
+if ($hash !== $payloadHash) {
+    // Kill the script or do something else here.
+    die('Bad secret');
+}
+
+// Your code here.
+exec('cd summon2;~/opt/bin/git pull');
+
+?>
