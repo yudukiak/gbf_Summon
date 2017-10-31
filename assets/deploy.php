329581dbae38e7cbe92ec1cb30f465190c82b0b8
micelle <micelle_yamame@hotmail.co.jp>
2017-10-31 17:57:54 +0900

    [add] Tweet function

--- assets/deploy.php
+++ assets/deploy.php
@@ -1,4 +1,8 @@
 <?php
+// Noticeエラーだけを非表示に設定
+error_reporting(E_ALL & ~E_NOTICE);
+
+// 必要な情報を取ってくる
 include '../../gbf_summon.php';
 
 // http://isometriks.com/verify-github-webhooks-with-php
@@ -22,7 +26,43 @@ if ($hash !== $payloadHash) {
     die('Bad secret');
 }
 
+// --------------------------------------------------------
+//
 // Your code here.
+//
+// --------------------------------------------------------
+
+// GitHubよりプル
 exec('cd ../;~/opt/bin/git pull');
+
+// JSONの処理
+$json = file_get_contents('php://input');
+$array = json_decode($json, true);
+// メッセージの処理
+$message = $array["commits"][0]["message"];
+$url     = $array["commits"][0]["url"];
+$pattern = "/\n+.+/"; // 対象
+$replace = "";        // 結果
+$message = preg_replace ($pattern, $replace, $message);
+$string  = "#フレ石編成的ななにか\n\n".$message."\n".$url;
+//thmOAuth.phpを読み込む
+require_once("../twitter/library/tmhOAuth.php");
+//初期設定
+$tmhOAuth = new tmhOAuth(array(
+  'consumer_key' => $API_Key,
+  'consumer_secret' => $API_Secret,
+  'user_token' => $Access_Token,
+  'user_secret' => $Access_Token_Secret,
+));
+//ツイートしたい文章
+$text = $string;
+//ツイートする
+$tweet_update = $tmhOAuth->request(
+  'POST', //リクエストの種類（POST/GETがある）
+  'https://api.twitter.com/1.1/statuses/update.json', //ツイート用のTwitter REST APIを指定
+  array(
+    'status' => $text //ツイートしたい文章を指定
+  )
+);
 
 ?>
