196c52f63824702231cd96d6b9e795380c9145fe
micelle <micelle_yamame@hotmail.co.jp>
2017-11-02 18:29:13 +0900

    [update] #2 デプロイの調整

--- deploy/change_deploy.php
+++ deploy/change_deploy.php
@@ -1,12 +1,15 @@
 <?php
-require_once "deploy.php";
+// ディレクトリを指定
+$gbfDir = realpath("../../");
+$mainDir = $gbfDir."/summon";
+$testDir = $gbfDir."/summon.test";
 
 header('Content-Type: application/json');
 $data = "{$_POST['branch']}へ切り替えました";
 $varBranch = (string)filter_input(INPUT_POST, 'branch');
 
 if($varBranch !== ""){
-  $command = "cd ".$testDir.";~/opt/bin/git fetch origin;~/opt/bin/git reset --hard origin/".$varBranch;
+  $command = "cd ".$testDir.";~/opt/bin/git fetch origin;~/opt/bin/git checkout --force origin/".$varBranch.";~/opt/bin/git reset --hard origin/".$varBranch;
   exec($command);
 }
 
