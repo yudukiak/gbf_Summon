6f31397a30967694a50839e5c406789dc4475e0d
micelle <micelle_yamame@hotmail.co.jp>
2018-03-23 16:57:31 +0900

    [fix] シュヴァ、セレの4凸に対応

--- assets/script.js
+++ assets/script.js
@@ -227,7 +227,7 @@ function table_display(){
           if(job_rarity.match(/^Djeeta$/)) return '_1_01.png';
           if(job_rarity.match(/^Gran$/)) return '_0_01.png';
           }
-          if (fclass.match(/^summon$/) && fid.match(/^20400(20|27|28|34)000$/)) {
+          if (fclass.match(/^summon$/) && fid.match(/^20400(20|27|28|34|46|47)000$/)) {
             if(summon_rank===void 0) return '.jpg';
             if(summon_rank.match(/^rank4$/)) return '_02.jpg';
           }
