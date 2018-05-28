cbbadb6357020fe4d928ed4591140c449328f070
micelle <micelle_yamame@hotmail.co.jp>
2018-05-28 18:24:45 +0900

    [update] phpの調整

--- assets/index.php
+++ assets/index.php
@@ -1,10 +1,14 @@
 <?php
   header("Content-Type: application/json");
   function sanitize_output($buffer) {
-    $buffer = preg_replace('/\r\n|\n|\r|\s/s', '', $buffer); // 改行＆スペースを削除
+    $buffer = preg_replace("/\r\n|\n|\r|\s/s", "", $buffer); // 改行＆スペースを削除
     return $buffer;
   }
   ob_start("sanitize_output");
-  $json = $_GET["json"];
-  include("./{$json}");
+  if ( isset($_GET["json"]) && preg_match("/^(echo|foxtrot)$/i", $_GET["json"]) ) {
+    $data = file_get_contents("./{$_GET["json"]}");
+  } else {
+    $data = json_encode(["res" => "negative"]);
+  }
+  echo $data;
 ?>
